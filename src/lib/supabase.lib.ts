51ae4c242a863f96c5e1258d05bc6930e3a7fb96
Guilherme Vieira <dev@guilhermevieira.dev>
2025-01-05 14:39:30 -0300

    fix: adjust build command

--- src/lib/supabase.lib.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-export const supabase = createClient(
-  'https://vvtwnawbwboowuvlksvj.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ2dHduYXdid2Jvb3d1dmxrc3ZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk2NDEwMzQsImV4cCI6MjA0NTIxNzAzNH0.5EBhk6GSRqaiA4RTY_npIKgPAErLBLpvFK4SD2R1pTY',
-);
