982d636a49d7b0aeac8f2845d949bff1c8f38c2c
Guilherme Vieira <dev@guilhermevieira.dev>
2025-01-10 01:14:36 -0300

    fix: adjust cron to 12 hours instead 3

--- src/modules/seed-players-experience/seed-players-experience.service.ts
+++ src/modules/seed-players-experience/seed-players-experience.service.ts
@@ -39,7 +39,7 @@ export class SeedPlayersExperienceService {
     Logger.log(`Daily experience created for ${character.name}`);
   }
 
-  @Cron('0 */3 * * *')
+  @Cron('0 */12 * * *')
   async seedPlayersExperience() {
     try {
       const { worlds } = await this.worldsService.getAllWorlds();
