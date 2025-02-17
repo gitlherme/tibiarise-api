1e623457e96b0618c59648131a505298c2708888
Guilherme Vieira <dev@guilhermevieira.dev>
2025-02-17 19:31:18 -0300

    fix: enable cors to just tibiarise.app

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   };
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: 'https://tibiarise.app',
   });
   await app.listen(process.env.PORT ?? 3000);
 }
