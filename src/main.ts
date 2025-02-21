1deb841b011f6dbaa0a89c13cb9a5f6fdb9ca344
Guilherme Vieira <dev@guilhermevieira.dev>
2025-02-20 21:11:00 -0300

    fix: add localhost to cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   };
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://tibiarise.app',
+    origin: ['https://tibiarise.app', 'http://localhost:3000'],
   });
   await app.listen(process.env.PORT ?? 3000);
 }
