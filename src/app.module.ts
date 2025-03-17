8a9be0c99fc51603d38d45995754fe4d3fe6af83
gitlherme <hi@guilhermevieira.dev>
2025-03-17 20:27:51 -0300

    feat: add http timeout

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,15 @@ import { ScheduleModule } from '@nestjs/schedule';
 import { CharacterModule } from './modules/character/character.module';
 import { DeleteOldExperiencesModule } from './modules/delete-old-experiences/delete-old-experiences.module';
 import { ExperienceByWorldModule } from './modules/experience-by-world/experience-by-world.module';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
+    }),
+    HttpModule.register({
+      timeout: 100000,
     }),
     ScheduleModule.forRoot(),
     SeedPlayersExperienceModule,
